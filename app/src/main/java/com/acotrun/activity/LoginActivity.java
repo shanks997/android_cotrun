3045a9bbd3329b9882b6b9feb4a7eaba396df128
caoxiaoguo <907915031@qq.com>
2019-03-21 16:14:48 +0800

    shanks：AlertDialog can't be set in the "new Thread()", so I put it in the (main)Thread.

--- app/src/main/java/com/acotrun/activity/LoginActivity.java
+++ app/src/main/java/com/acotrun/activity/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.acotrun.activity;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -11,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.acotrun.R;
@@ -26,8 +27,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button btn_forgetPW;
     private Button btn_login;
     private Button btn_register;
-
-    private TextView tv_test;
 
     Boolean flag;
 
@@ -45,14 +44,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
 
-        tv_test = findViewById(R.id.test);
-
         btn_look.setOnClickListener(this);
         btn_forgetPW.setOnClickListener(this);
         btn_login.setOnClickListener(this);
         btn_register.setOnClickListener(this);
 
-        iv_cancel.bringToFront();
+//        iv_cancel.bringToFront();
         clearButtonListener(edt_id, edt_pwd, iv_cancel);
     }
 
@@ -62,29 +59,57 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(this, ForgetPwdActivity.class);
             startActivity(intent);
         } else if (v.getId() == R.id.btn_login) { // 点击了“登录”按钮
-            if (edt_id.length() < 11) { // 手机号码不足11位
-                Toast.makeText(this, "请输入正确的手机号", Toast.LENGTH_SHORT).show();
+            if (edt_id.length() == 0) {
+                Toast.makeText(this, "账号不能为空", Toast.LENGTH_SHORT).show();
                 return;
-            } else new Thread() {
-            // 匿名对象 形式为：new NoNameObject().fun();
-                @Override
-                public void run() {
-                    try {
-                        String sid = edt_id.getText().toString();
-                        String spwd = edt_pwd.getText().toString();
-                        flag = NetInfoUtil.isUser(sid, spwd);
-                        if (flag) {
-                            tv_test.setText("flag1 = " + flag);
-                        } else {
-                            tv_test.setText("flag2 = " + flag);
+            } else if (edt_pwd.length() == 0) {
+                Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                Thread td = new Thread() {
+                // 匿名对象 形式为：new NoNameObject().fun();
+                    @Override
+                    public void run() {
+                        try {
+                            String sid = edt_id.getText().toString();
+                            String spwd = edt_pwd.getText().toString();
+                            flag = NetInfoUtil.isUser(sid, spwd);
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
-                    } catch (Exception e) { e.printStackTrace(); }
+                    }
+                };
+                td.start();
+                // 让 td 线程先运行完，再运行主线程
+                try {
+                    td.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }.start();
-        } else if (v.getId() == R.id.btn_register) {
+                if (flag) {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    edt_id.setText("");
+                    edt_pwd.setText("");
+                    // Alert 对话框放在 td 线程中会显示不出来
+                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                    alert.setTitle("Alert!");
+                    alert.setMessage("账号密码有误，登陆失败！");
+                    alert.setNegativeButton("确定",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            });
+                    // 显示
+                    alert.show();
+                }
+            }
+        } else if (v.getId() == R.id.btn_register) { // 点击了“注册”按钮
             Intent intent = new Intent(this, RegisterActivity.class);
             startActivity(intent);
-        } else if (v.getId() == R.id.btn_look) {
+        } else if (v.getId() == R.id.btn_look) { // 点击了“随便看看”按钮
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
